e377442584351063e5d175b2c8eccd0fb0ee3e54
The Fire Owl <61467254+Owl-Man@users.noreply.github.com>
2025-03-21 12:12:36 +0300

    Update sections.vue

--- components/header/sections.vue
+++ components/header/sections.vue
@@ -33,11 +33,28 @@ export default{
     align-items: center;
     width: 1000px;
 }
+
+.sections > div:not(:last-child) {
+    position: relative;
+    margin-right: 20px;
+}
+
+.sections > div:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: -100px;
+    width: 2px;
+    height: 100%;
+    background-color: #000000;
+}
+
 .sectionsBtn {
     font-size: $headerFontSize;
     transition: transform 0.3s ease;
 }
+
 .sectionsBtn:hover {
-  transform: scale(1.2);
+    transform: scale(1.2);
 }
 </style>
